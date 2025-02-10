20ddccb4cd2d36037e0b44afeedf96946ff53f25
ArmanGrewal007 <armansinghgrewal@gmail.com>
2025-02-11 01:22:07 +0530

    little formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#007AFF",
-        secondary: "#6C757D",
-      },
-    },
-  },
+  theme: { extend: { colors: { primary: "#007AFF", secondary: "#6C757D" } } },
   plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ghPages } from "vite-plugin-gh-pages";
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), ghPages()],
     base: mode == "production" ? "/repo_name/" : "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   };
 });
